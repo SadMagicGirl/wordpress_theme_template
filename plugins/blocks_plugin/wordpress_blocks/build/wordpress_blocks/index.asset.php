6f876a750cb2cc850426bb9324db8b67d64f43a4
SadMagicGirl <lizalove8426@gmail.com>
2025-05-19 19:35:39 +0300

    remove themes/plugins

--- plugins/blocks_plugin/wordpress_blocks/build/wordpress_blocks/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '270112ec78fb1a9f9210');
--- plugins/blocks_plugin/wordpress_blocks/build/wordpress_blocks/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '7b44856e2540baf1007a');
